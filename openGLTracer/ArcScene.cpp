0f2b0278d450cdb21cb404fa05483f3fa13cc9d6
Arcob <1241994554@qq.com>
2018-12-20 21:57:05 +0800

    adding scene

--- openGLTracer/ArcScene.cpp
+++ openGLTracer/ArcScene.cpp
@@ -2,5 +2,19 @@
 
 
 namespace Arc_Engine {
+	const std::shared_ptr<Arc_Engine::DirectionLight> ArcScene::light() const{
+		return _light;
+	}
 
+	void ArcScene::setLight(std::shared_ptr<Arc_Engine::DirectionLight> light) {
+		_light = light;
+	}
+
+	void ArcScene::addGameObject(std::shared_ptr<Arc_Engine::ArcGameObject> gameObject) {
+		_sceneGameObjectObjects.push_back(gameObject);
+	}
+
+	std::vector<std::shared_ptr<ArcGameObject>> ArcScene::getGameObjectsInScene() {
+		return _sceneGameObjectObjects; // 返回的是vector的复制
+	}
 }
--- openGLTracer/ArcScene.h
+++ openGLTracer/ArcScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include "ArcGameObject.h"
+#include "LightBundle.h"
 
 namespace Arc_Engine {
 
@@ -10,12 +11,13 @@ namespace Arc_Engine {
 		ArcScene() = default;
 		~ArcScene() = default;
 		std::vector<std::shared_ptr<ArcGameObject>> getGameObjectsInScene();
-		void AwakeAllGameObjects();
-		void StartAllGameObjects();
-		void UpdateAllGameObjects();
-	
+		const std::shared_ptr<Arc_Engine::DirectionLight> light() const;
+		void setLight(std::shared_ptr<Arc_Engine::DirectionLight> light); //之后把light改成基类？
+		void addGameObject(std::shared_ptr<Arc_Engine::ArcGameObject> gameObject);
+
 	private:
 		std::vector<std::shared_ptr<ArcGameObject>> _sceneGameObjectObjects;
+		std::shared_ptr<Arc_Engine::DirectionLight> _light;
 	};
 }
 
