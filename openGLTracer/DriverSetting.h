e0665e5bf5d15ba67418cecc530b12de99808307
Arcob <1241994554@qq.com>
2019-02-03 13:23:32 +0800

    update gif

--- openGLTracer/DriverSetting.h
+++ openGLTracer/DriverSetting.h
@@ -3,7 +3,7 @@
 #include "common.h"
 #include "BoxMoverApplication.h"
 #define Application BoxMoverApplication
-#define EnableTest true
+#define EnableTest false
 #define EnableShadow true
 
 const GLuint WIDTH = 1024, HEIGHT = 1024;
